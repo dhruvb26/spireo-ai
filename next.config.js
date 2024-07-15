302789018345504a5b034bbf654fc88cbc887252
Dhruv Bansal <108138649+dhruvbansal26@users.noreply.github.com>
2024-07-15 17:07:00 +0530

    trying to fix

--- next.config.js
+++ next.config.js
@@ -6,30 +6,6 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.pixabay.com",
-      },
-      {
-        protocol: "https",
-        hostname: "media.licdn.com",
-      },
-    ],
-  },
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
