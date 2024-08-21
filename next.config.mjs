6637ffc87eb67017b903573a3d3bde65ea776e82
Dhruv Bansal <108138649+dhruvbansal26@users.noreply.github.com>
2024-08-21 00:17:15 -0400

    input for rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,10 @@ const config = {
     return config;
   },
   images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+
     remotePatterns: [
       {
         protocol: "https",
